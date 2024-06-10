6fdb29bbf082e3bd4da4426f5ab107e8ac4bb6bc
=patelsudhakar787 <=patelsudhakar787@gmail.com>
2024-06-10 23:52:05 +0530

    Streamlit Interative App

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,4 +1,22 @@
 import streamlit as st
 
-st.title("Hello StreamLit")
-st.write("This is Simple Streamlit app")
+# st.title("Hello StreamLit")
+# st.write("This is Simple Streamlit app")
+
+st.title("Interative Streamlit App")
+
+#Input for Name
+name = st.text_input("Enter your Name")
+
+if name:
+    st.write(f'Hello, {name}!')
+
+#Select Number from Silder
+number = st.number_input('Select a Number', 0 , 100, 50)
+if number:
+    st.write(f'Select Number is, {number}')
+
+if st.button('Greet'):
+    st.write('Greeting from Streamlit')
+
+    
